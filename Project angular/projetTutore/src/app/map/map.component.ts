20eceec0b170247dbb1dc8d88e1dd5245c8971ba
paulinecazalis <cazapauline@gmail.com>
2021-03-23 18:13:00 +0100

    update map

--- Project angular/projetTutore/src/app/map/map.component.ts
+++ Project angular/projetTutore/src/app/map/map.component.ts
@@ -14,17 +14,17 @@ export class MapComponent implements OnInit{
   private map: L.Map | L.LayerGroup<any> | undefined;
   data = [];
   dataCapt1: any;
-  marker: any;
+  //marker: any;
   latlgn: any;
 
   smallIcon = new L.Icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconUrl: '../assets/delete.png',
     iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
-    iconSize:    [25, 41],
+    iconSize:    [25, 25],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    shadowSize:  [41, 41]
+    shadowSize:  [35, 33]
   });
 
   greenIcon = new L.Icon({
@@ -38,6 +38,15 @@ export class MapComponent implements OnInit{
 
   orangeIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
@@ -63,14 +72,10 @@ export class MapComponent implements OnInit{
       response=>{
         this.dataCapt1 = response.handsetCards;
         const container = L.DomUtil.get('map');
-        //console.log(container.leaflet_id);
         if (container != null) {
           this.map?.off();
           this.map?.remove();
-          //container._leaflet_id = null;
-          //this.initMap();
         }
-        console.log(container);
         this.initMap();
         
       },
@@ -78,7 +83,6 @@ export class MapComponent implements OnInit{
         alert('Impossible de recevoir les données du serveur');
       });
       //this.initMap();
-      console.log('ngoninit map');
   }
 
   private initMap(): void {
@@ -93,7 +97,33 @@ export class MapComponent implements OnInit{
     });
  
     tiles.addTo(this.map);
+
+    const marker = L.marker([this.dataCapt1[0].Lat, this.dataCapt1[0].Long], { icon: this.greenIcon }).addTo(this.map);
+    marker.bindPopup("Pression :"+ this.dataCapt1[0].Pression, {minWidth: 100, maxHeight: 1000});
+    const marker2 = L.marker([43.31612921579041, -0.3604853135427254], { icon: this.greenIcon }).addTo(this.map);
+    marker2.bindPopup("Pression :"+ this.dataCapt1[1].Pression, {minWidth: 100, maxHeight: 1000});
+    const marker3 = L.marker([43.31393383437617, -0.37402919927750927], { icon: this.greenIcon }).addTo(this.map);
+    marker3.bindPopup("Pression :"+ this.dataCapt1[2].Pression, {minWidth: 100, maxHeight: 1000});
+
+    if(this.dataCapt1[0].Pression >= this.dataCapt1[0].Seuil){
+      marker.setIcon(this.redIcon);
+    }else if (this.dataCapt1[0].Pression > this.dataCapt1[0].Seuil/2){
+      marker.setIcon(this.orangeIcon);
+    }
+    if(this.dataCapt1[1].Pression >= this.dataCapt1[1].Seuil){
+      marker2.setIcon(this.redIcon);
+    }else if (this.dataCapt1[1].Pression > this.dataCapt1[1].Seuil/2){
+      marker2.setIcon(this.orangeIcon);
+    }
+    if(this.dataCapt1[2].Pression >= this.dataCapt1[2].Seuil){
+      marker3.setIcon(this.redIcon);
+    }else if (this.dataCapt1[2].Pression > this.dataCapt1[2].Seuil/2){
+      marker3.setIcon(this.orangeIcon);
+    }
+
     console.log(this.dataCapt1);
+    
+    
   }
 
   /*createMap() {
@@ -123,7 +153,5 @@ export class MapComponent implements OnInit{
     //https://github.com/pointhi/leaflet-color-markers
   }*/
 
-  constructor(private breakpointObserver: BreakpointObserver, public appService: AppService) {
-    console.log('constructor map');
-  }
+  constructor(private breakpointObserver: BreakpointObserver, public appService: AppService) {}
 }
